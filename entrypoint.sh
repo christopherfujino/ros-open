4e6890efb7bb6e547b4a07a593ddfa423e3f902b
Christopher Fujino <christopherfujino@gmail.com>
2026-03-05 13:07:57 -0800

    enable /status page for server stats

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ trap fallback_to_shell INT
 echo 'About to spawn lighttpd...'
 # -D - Don't get to background
 #lighttpd -D -f /etc/lighttpd/lighttpd.conf
-lighttpd -f /etc/lighttpd/lighttpd.conf
+lighttpd -f /etc/lighttpd/lighttpd.conf || (/bin/bash && false)
 
 echo 'End.'
 
