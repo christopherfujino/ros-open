4afd20e1305066932e183621d059fd86824b2008
Christopher Fujino <christopherfujino@gmail.com>
2026-03-23 19:02:26 +0000

    update ttyd.sh

--- ttyd.sh
+++ ttyd.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-ttyd --writable --cwd "$HOME" --credential foo:bar /bin/bash
+set -euo pipefail
+
+ROOT="$(dirname "$(realpath "${BASH_SOURCE[0]}")" )"
+PID_FILE="${ROOT}/ttyd.pid"
+
+cd "$ROOT"
+
+# --credential user:pw
+ttyd --writable --cwd "$HOME" /bin/bash &
+PID=$!
+
+echo "Forked ttyd as the background process $PID"
+echo "$!" > "$PID_FILE"
+
+wait $!
+
+rm "$PID_FILE"
